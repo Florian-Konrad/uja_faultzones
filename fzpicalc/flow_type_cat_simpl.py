45c957155ab8b649e3a408176c780e9a4417b31b
Florian Konrad <florian.ko@protonmail.com>
2022-01-30 12:38:40 +0100

    code clean up

--- fzpicalc/flow_type_cat_simpl.py
+++ fzpicalc/flow_type_cat_simpl.py
@@ -5,187 +5,139 @@ from time import time
 
 
 def time_s_to_h(t_s,t_ini): #Berechne Zeitschritte in dt seit Foerderbeginn in Stunden um
+
     t_h = (t_s-t_ini)/3600.0
+    
     return t_h
 
-
 def calc_dP(P_trans,P_ini): #Berechne Druckaenderung seit Foerderbeginn
+
     dP = P_ini-P_trans
+    
     return dP
 
-
 def calc_DER(dP_before,dP_after,th_before,th_after): #Berechne Druck-Derivat
+
     DER = (dP_after-dP_before)/(np.log(th_after)-np.log(th_before)) #np.log = ln ; np.lo10 = log
+    
     return DER
 
 def calc_storage(poro,kfl=5.0e+8):     #berechne den speicherkoeffizienten aus fixed kfl 5e8 und input poro
+
     stor = (poro)/(kfl)    #spk=p/kfl einheit:Pa
+    
     return stor
 
 def calc_ratio(vm,vf):      # berechne das Verhaeltnis einer matrixvariablen zu einer stoerungsvariablen
+
     r = vf/(vm+vf)               #r>1 bedeutet matrixvariable groeser als stoerungsvariable
+    
     return r
 
 def calc_logslope(y1,y2,x1,x2):    #berechnet die Steigung im Doppellogplot
+
     slope = (np.log10(y1) - np.log10(y2)) / (np.log10(x1) - np.log10(x2))
+    
     return slope
 
 def calc_semilogslope(y1,y2,x1,x2):    #berechnet die Steigung im Semilogplot
+
     slope = (y1 - y2) / (np.log10(x1) - np.log10(x2))
+    
     return slope
 
 def calc_linearslope(y1,y2,x1,x2):    #berechnet die Steigung im normalen linearen Plot
+
     slope = (y1 - y2) / (x1 - x2)
+    
     return slope
 
-
-
-
 def append_linear_slope(df,variablename):
-    entry_length = len(df)
-    n = 1
-    df.loc[:,variablename+'_slope'] = np.nan
-    while n < (entry_length - 1):
+        
+    for n in range(1,len(df) - 1):
         y1 = df[variablename][n-1]
         y2 = df[variablename][n+1]
         t1 = df['t_hour'][n-1]
         t2 = df['t_hour'][n+1]
         slope = calc_linearslope(y1,y2,t1,t2)
-        #df['DER_slope'][n] = derivative_slope
         df.loc[n,variablename+'_slope'] = slope
-        n += 1
+        
     return df
 
-
-
-
 def append_th_file(df):
-    entry_length = len(df)
-    n = 0
+    
     b = df['time'][0]
-    df.loc[:,'t_hour'] = np.nan
-    while n < entry_length:
+    for n in range(0,len(df)):
         a = df['time'][n]
         t_h = time_s_to_h(a,b)
         df.loc[n,'t_hour'] = t_h
-        n += 1
+
     return df
 
-def fast_append_th_file(df):
+def apply_append_th_file(df):
+    
     b = df['time'][0]
     df['t_hour'] = df.apply(lambda row: time_s_to_h(row['time'],b), axis=1)
+    
     return df
-
 
 def append_dP_file(df):
-    entry_length = len(df)
-    n = 1  # 1 hier weil ich dp = 0 in der Liste haben moechte! Zur spaeteren Darstellung!
+    
     b = df['FoerderDruck'][0]
-    df.loc[:,'DruckAenderung'] = np.nan
-    while n < entry_length:
+    for n in range(1,len(df)): # 1 hier weil dp = 0 in der Liste sein soll zur spaeteren Darstellung!
         a = df['FoerderDruck'][n]
         dP = calc_dP(a,b)
-        #df['DruckAenderung'][n] = dP
         df.loc[n,'DruckAenderung'] = dP
-        n += 1
+        
     return df
 
-def fast_append_dP_file(df):
+def apply_append_dP_file(df):
+    
     b = df['FoerderDruck'][0]
     df['DruckAenderung'] = df.apply(lambda row: calc_dP(row['FoerderDruck'],b), axis=1)
+    
     return df
 
 def append_DER_file(df):
-    entry_length = len(df)
-    n = 2
-    df.loc[:,'DER'] = np.nan
-    df.loc[:,'dP_slope'] = np.nan
-    df.loc[:,'dP_slope_semilog'] = np.nan
-    while n < entry_length: #the 2 was changed from 1 due to RBoutput drop at last points
+      
+    for n in range(2,len(df)-1):
         p1 = df['DruckAenderung'][n-1]
         p2 = df['DruckAenderung'][n+1]
         t1 = df['t_hour'][n-1]
         t2 = df['t_hour'][n+1]
-
-        if n < (entry_length - 2):
+        
+        if n < (len(df) - 2):
             df.loc[n,'DER'] = calc_DER(p1,p2,t1,t2)
 
-        if n > 1 and n < (entry_length - 1):
+        if n > 1 and n < (len(df) - 1):
             df.loc[n,'dP_slope'] = calc_logslope(p1,p2,t1,t2)
             df.loc[n,'dP_slope_semilog'] = calc_semilogslope(p1,p2,t1,t2)
-
-        n += 1
+    
     return df
-
-def fast_append_DER_file(df):
-    ddP = df.loc[:,'DruckAenderung'].values
-    th = df.loc[:,'t_hour'].values
-    DER = [np.nan]*len(th)
-    dP_slope = [np.nan]*len(th)
-    dP_slope_semilog = [np.nan]*len(th)
-    for i, eachDER in enumerate(DER,start=2):
-        if i < (len(DER)-1):
-            p1 = ddP[i-1]
-            p2 = ddP[i+1]
-            t1 = th[i-1]
-            t2 = th[i+1]
-            if i < len(DER)-2:
-                DER[i] = calc_DER(p1,p2,t1,t2)
-            if i > 1:
-                dP_slope[i] = calc_logslope(p1,p2,t1,t2)
-                dP_slope_semilog[i] = calc_semilogslope(p1,p2,t1,t2)
-    df.loc[:,'DER'] = DER
-    df.loc[:,'dP_slope'] = dP_slope
-    df.loc[:,'dP_slope_semilog'] = dP_slope_semilog
-    return df
-
-
 
 def append_slope_file(df):
-    entry_length = len(df)
-    n = 3
-    df.loc[:,'DER_slope'] = np.nan
-    while n < (entry_length - 3):
+        
+    for n in range(3,len(df)-3):
         y1 = df['DER'][n-1]
         y2 = df['DER'][n+1]
         t1 = df['t_hour'][n-1]
         t2 = df['t_hour'][n+1]
-        derivative_slope = calc_logslope(y1,y2,t1,t2)
-        #df['DER_slope'][n] = derivative_slope
-        df.loc[n,'DER_slope'] = derivative_slope
-        n += 1
-    return df
-
-def fast_append_slope_file(df):
-    DER = df.loc[:,'DER'].values
-    th = df.loc[:,'t_hour'].values
-    DER_slope = [np.nan]*len(th)
-    for i, eachDERslope in enumerate(DER_slope,start=3):
-        if i < len(DER)-3:
-            p1 = DER[i-1]
-            p2 = DER[i+1]
-            t1 = th[i-1]
-            t2 = th[i+1]
-            DER_slope[i] = calc_logslope(p1,p2,t1,t2)
-    df.loc[:,'DER_slope'] = DER_slope
+        df.loc[n,'DER_slope'] = calc_logslope(y1,y2,t1,t2)
+    
     return df
 
 def append_pressure_slope_file(df):
-    entry_length = len(df)
-    n = 2
-    df.loc[:,'dP_slope'] = np.nan
-    df.loc[:,'dP_slope_semilog'] = np.nan
-    while n < (entry_length - 1):
+    
+    for n in range(2,len(df)-1):
         y1 = df['DruckAenderung'][n-1]
         y2 = df['DruckAenderung'][n+1]
         t1 = df['t_hour'][n-1]
         t2 = df['t_hour'][n+1]
         dP_slope = calc_logslope(y1,y2,t1,t2)
         dP_slope_semilog = calc_semilogslope(y1,y2,t1,t2)
-        #df['dP_slope'][n] = dP_slope
         df.loc[n,'dP_slope'] = dP_slope
         df.loc[n,'dP_slope_semilog'] = dP_slope_semilog
-        n += 1
+    
     return df
 
 
@@ -205,6 +157,10 @@ def real_linear_marker(df,slope=0.5,delta=0.1):
                 df.loc[n,'real_linear_marker'] = df['DER_slope'][n]
         n += 1
     return df
+
+
+
+
 
 def fast_linear_bilinear_steep_neg_pos_marker(df):
     linear_slope = 0.5
@@ -682,21 +638,246 @@ def append_flow_typ(df,matrix_perm):
     return df, faultzone_bd_detected
 
 
+#APPEND FLOW TYPE TO DruckAenderung.csv
+def append_flow_typ_new(df,matrix_perm):
+    entry_length = len(df)
+    df.loc[:,'flowtype_new'] = 'other'
+    
+    for n in range(0,entry_length):
+        if df['DER_slope_pos'][n] != 0:
+            df.loc[n,'flowtype_new'] = 'positive'
+        if df['DER_slope_neg'][n] != 0:
+            df.loc[n,'flowtype_new'] = 'negative'
+    
+    for n in range(0,entry_length):
+        #only use if more then two consecutive points show bilinear behavior
+        if df['bilinear_marker'][n] != 0 and len(df.loc[n-2:n+2].loc[df['bilinear_marker'] > 0.0]) > 2:
+
+            #safty check slopes must be similar:
+            DER_dP_comp_n = np.isclose(df['DER_slope'][n],df['dP_slope'][n],rtol=0.15)
+            DER_dP_comp_n1 = np.isclose(df['DER_slope'][n+1],df['dP_slope'][n+1],rtol=0.15)
+            DER_dP_comp_n2 = np.isclose(df['DER_slope'][n+2],df['dP_slope'][n+2],rtol=0.15)
+            if DER_dP_comp_n == True and DER_dP_comp_n1 == True and DER_dP_comp_n2 == True:
+                #more safty: DER and dP can't be to far away from each other
+                if df['DruckAenderung'][n] < 5*df['DER'][n]:
+                    df.loc[n,'flowtype_new'] = 'bilinear'
+
+    
+    for n in range(0,entry_length):
+        if df['greater_than_0.6'][n] != 0:
+            df.loc[n,'flowtype_new'] = 'steep'
+        if df['BC_slope'][n] != 0:
+            df.loc[n,'flowtype_new'] = 'model_boundary'
+        if (matrix_perm < 1e-14) & (df['BC_slope'][n] != 0):
+            df.loc[n,'flowtype_new'] = 'steep'
+
+    for n in range(1,entry_length):
+        if df['well_slope'][n] != 0:
+            df.loc[n,'flowtype_new'] = 'well_effect'
+        else:
+            # check if current n is already clasified as steep
+            # but one index previous it was still well_effect
+            # change to well_effect since its just a transition
+            if df.loc[n-1,'flowtype_new'] == 'well_effect' and df.loc[n,'flowtype_new'] == 'steep':
+                df.loc[n,'flowtype_new'] = 'well_effect'
+
+
+    #if we have well_effect following steep change all well_effect to steep
+    # only if dP solpe in the beginning  flat (<0.3) --> fault zone has matrix boundary
+    # if it is not flat it still is a well effect
+
+    #to do : change 0.3 to 0.7 or check for no of well_effect > 50 --> steep
+    if (df.loc[:44,'dP_slope'] < 0.3).any() == True or len(df.loc[df['flowtype_new']=='well_effect']) > 50:
+        for n in range(10,entry_length):
+            if df['flowtype_new'][n] == 'well_effect' and df['flowtype_new'][n-1] == 'steep':
+                # if true go forward and replace until well effect stops
+                for d_n in range(n,entry_length):
+                    if df['flowtype_new'][d_n] == 'well_effect':
+                        df.loc[d_n,'flowtype_new'] = 'steep'
+                    else:
+                        #will be triggert if well effect stops --> stopping replacement completely
+                        break
+                #then stop outer search:
+                break
+        
+
+
+    for n in range(0,entry_length):
+        
+        if df['DER_slope_radial'][n] != 99:
+            df.loc[n,'flowtype_new'] = 'radial'
+
+        if df['real_linear_marker'][n] != 0 and len(df.loc[n-2:n+2].loc[df['real_linear_marker'] > 0.0]) > 2:
+            #safty check slopes must be similar:
+            DER_dP_comp_n = np.isclose(df['DER_slope'][n],df['dP_slope'][n],rtol=0.15)
+            DER_dP_comp_n1 = np.isclose(df['DER_slope'][n+1],df['dP_slope'][n+1],rtol=0.15)
+            if DER_dP_comp_n == True and DER_dP_comp_n1 == True:
+                if matrix_perm > 1e-14:
+                    #check if model boundary found before this point, if yes --> RB model behavior no real linear flow
+                    if not (df.loc[10:n,'flowtype_new'] == 'model_boundary').any():
+                        df.loc[n,'flowtype_new'] = 'linear'
+                else:
+                    df.loc[n,'flowtype_new'] = 'linear'
+        
+        
+
+
+    # check last points of already specified flowtype and check if type changes more than three times
+    # --> looks like reduced basis method error
+    # should be removed and changed to other:
+    #last_n_rows = 15
+    #if len(df.tail(last_n_rows)['flowtype_new'].unique()) > 3:
+    #    df.iloc[-last_n_rows:,df.columns.get_loc('flowtype_new')].loc[df['flowtype_new']!='model_boundary'] = 'other'
+
+    # secondary check necessary
+    # above check can fail  --> then model_bd + other + e.g. linear flow type can be
+    # at the end of df
+    # check last 22 rows to see if they contain model_boundary
+    # if so change other flowtypes to other:
+    last_n_rows = 22
+    if 'model_boundary' in df.tail(last_n_rows)['flowtype_new'].to_list():
+        df.iloc[-last_n_rows:,df.columns.get_loc('flowtype_new')].loc[df['flowtype_new']!='model_boundary'] = 'other'
+
+
+
+    #repair bi-/linear after steep stiuations
+    #where fz reacts first as bc but the stabilzes in bilinear or linear flow regime
+    #happens only with t_hour > 0.2 = index > 44
+    #account only for situations with more than 6 points of bi-/linear flow
+
+
+
+
+
+    # search and pick bi-/linear parts after steep
+
+    #if found this will store all lists relevant index ranges
+    #type list of lists
+    potential_indexlist_list = []
+    keywords = ['linear','bilinear']
+    
+    for n in range(44,entry_length-1):
+        
+        if df['flowtype_new'][n] == 'steep':
+            #CHECK THE FOLLOWING POINT
+            start_i = 0
+            end_i = 0
+            for each_key in keywords:
+                if df['flowtype_new'][n+1] == each_key:
+                    #criterium found now endpoint of found flowregime
+                    local_i = n+1
+                    start_i = local_i
+                    
+                    for n_l in range(local_i,entry_length):
+                        if df['flowtype_new'][n_l] != each_key:
+                            end_i = n_l-1
+                        
+                            #now check if more than 6 points in that flow regime follow
+                            if end_i-start_i+1 > 6:
+                                c_indexlist = range(start_i,end_i+1)
+                                potential_indexlist_list.append(c_indexlist)
+                            #end local search
+                            break
+                    
+                    break
+            
+            #often it is the case that steep is followed by positive and then bi-/linear
+            if df['flowtype_new'][n+1] == 'positive':
+                
+                for n_l in range(n+2,entry_length-1): #+2 to check cell after the detected 'positive'
+                    for each_key in keywords:
+                        if df['flowtype_new'][local_i] == each_key:
+                            start_i = n_l
+                            
+                            for n_l_l in range(n_l,entry_length):
+                                if df['flowtype_new'][n_l_l] != each_key:
+                                    #flow type not found anymore --> take index befor and save as last flowtype entry
+                                    end_i = n_l_l-1
+
+                                    #now check if more than 6 points in that flow regime follow
+                                    if end_i-start_i+1 > 3:
+                                        c_indexlist = range(start_i,end_i+1)
+                                        potential_indexlist_list.append(c_indexlist)
+                                    
+                                    #end local local search
+                                    break
+                            #end keywords loop
+                            break
+                    #end outer for loop with n_l index
+                    break
+
+                             
+
+
+    confirmed_index_lists = []
+
+    # check for those points if dp_slope changes less than 0.001 in more than 2 obersvations
+    if len(potential_indexlist_list) > 0:
+        for each in potential_indexlist_list:
+            df_isolated = df.loc[each].reset_index()
+
+            #calc dP_slope_slope:
+            append_linear_slope(df_isolated,'dP_slope')
+
+            #check if DER slope and dP slope are similar
+            DER_dP_comp = np.isclose(df_isolated['DER_slope'],df_isolated['dP_slope'],rtol=0.15)
+
+            if len(each) > 10:
+                #test criteria:
+                testing_confirmation = abs(df_isolated['dP_slope_slope']) < 0.02
+                if True in testing_confirmation.to_list() and True in DER_dP_comp:
+                    if testing_confirmation.value_counts()[True] > 1:
+                        confirmed_index_lists.append(each)
+            else:
+                #test criteria:
+                testing_confirmation = abs(df_isolated['dP_slope_slope']) < 0.001
+                if True in testing_confirmation.to_list() and True in DER_dP_comp:
+                    if testing_confirmation.value_counts()[True] > 1:
+                        confirmed_index_lists.append(each)
+
+
+
+    faultzone_bd_detected = False
+
+    if len(confirmed_index_lists) > 0:
+
+        faultzone_bd_detected = True
+
+        for each in confirmed_index_lists:
+            
+            #get original df index of first bi-/linear flow entry
+            rep_end = each[0]-1
+            rep_start = 0
+            
+            #go back over original df, find indices to repair:
+            for rep_i in range(0,each[0]-1,-1):
+                if df['flowtype_new'][rep_i] == 'steep' and df['flowtype_new'][rep_i-1] != 'steep':
+                    rep_start = rep_i
+                    break
+
+            #take rep_indices and change entries to positive flowtype:
+            rep_indices = range(rep_start,rep_end+1)
+            df.loc[rep_indices,'flowtype_new'] = 'positive'
+
+            #print('repair done')
+
+
+    return df, faultzone_bd_detected
+
+
 
 
 
 def classify_datapoints(df_p_curve,params_for_p_curve):
     df = df_p_curve
     if not 't_hour' in list(df_p_curve.columns):
-        #df = append_th_file(df)
-        df = fast_append_th_file(df)
+        df = append_th_file(df)
+        #df = apply_append_th_file(df)
     if not 'DruckAenderung' in list(df_p_curve.columns):
-        #df = append_dP_file(df)
-        df = fast_append_dP_file(df)
-    #df = append_DER_file(df)
-    df = fast_append_DER_file(df)
-    #df = append_slope_file(df)
-    df = fast_append_slope_file(df)
+        df = append_dP_file(df)
+        #df = apply_append_dP_file(df)
+    df = append_DER_file(df)
+    df = append_slope_file(df)
     #df = append_pressure_slope_file(df) #not neede anymore got integrated into append_DER_file()
 
     #df = real_linear_marker(df)
@@ -710,6 +891,9 @@ def classify_datapoints(df_p_curve,params_for_p_curve):
     #df = identify_bc(df)
     df = identify_radial(df)
     df, faultzone_bd_detected = append_flow_typ(df,params_for_p_curve['k_matrix'])
+    df, faultzone_bd_detected_new = append_flow_typ_new(df,params_for_p_curve['k_matrix'])
+    if faultzone_bd_detected_new != faultzone_bd_detected:
+        print('faultzone bd detection different for new function version')
     return df, faultzone_bd_detected
 
 
